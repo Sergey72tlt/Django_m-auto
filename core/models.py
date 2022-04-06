926124a8e5a0352dbfff724fe6d111a0bbdecbd2
Sergey72tlt <94053785+Sergey72tlt@users.noreply.github.com>
2022-04-06 18:44:39 +0400

    Create profiles

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+def user_avatar_path(instance, filename):
+    user_id = instance.user.id
+    return f'user_{user_id}/avatar/{filename}'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    birth_date = models.DateField(blank=True, null=True)
+    about = models.TextField(max_length=500, blank=True)
+    avatar = models.ImageField(upload_to=user_avatar_path)
+    friends = models.ManyToManyField(User, blank=True, related_name='friends')
+
+    def __str__(self):
+        return f'Profile of {self.user.username}'
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 # from django.conf.urls import url
 from django.urls import path
-from .views import index, posts
+from .views import index
 
 urlpatterns = [
     path('', index, name='index'),
