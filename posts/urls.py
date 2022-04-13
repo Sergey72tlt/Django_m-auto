ca57fda2cb3655759fe58a3854374bee8b870b18
Sergey72tlt <94053785+Sergey72tlt@users.noreply.github.com>
2022-04-13 19:37:09 +0400

    Create Views

---
+++ posts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import index, feed, post_detail, post_create, post_edit, post_delete, post_favorite
+
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('feed/', feed, name='feed'),
+    path('posts/<int:post_id>', post_detail, name='post-detail'),
+    path('posts/<int:post_id>/edit/', post_edit, name='post-edit'),
+    path('posts/<int:post_id>/delete/', post_delete, name='post-delete'),
+    path('posts/<int:post_id>/favorite/', post_favorite, name='post-favorite'),
+    path('posts/create/', post_create, name='post-create'),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def feed(request):
+    return HttpResponse('Список объявлений')
+
+
+def post_detail(request, post_id):
+    responce = f'Детальное представление объявления #{post_id}'
+    return HttpResponse(responce)
+
+
+def post_edit(request, post_id):
+    responce = f'Редактирование объявления #{post_id}'
+    return HttpResponse(responce)
+
+
+def post_create(request):
+    return HttpResponse('Создание нового объявления')
+
+
+def post_delete(request, post_id):
+    responce = f'Удаление объявления #{post_id}'
+    return HttpResponse(responce)
+
+
+def post_favorite(request, post_id):
+    responce = f'Объявление добавлено в избранное #{post_id}'
+    return HttpResponse(responce)
