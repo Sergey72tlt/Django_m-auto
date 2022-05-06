938ef6a8b46a16e45535eeaf7bf2f367c8659299
Sergey72tlt <94053785+Sergey72tlt@users.noreply.github.com>
2022-05-06 14:49:54 +0400

    Create new post

--- posts/models.py
+++ posts/models.py
@@ -25,10 +25,10 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    posts = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     text = models.CharField(max_length=100)
     date_pub = models.DateTimeField(auto_now_add=True)
     date_edit = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Author - {self.author.username}, publicated - {self.date_pub}, post - {self.post.description[:15]}'
+        return f'Author - {self.author.username}, publicated - {self.date_pub}, post - {self.post.description[:15]}...'
