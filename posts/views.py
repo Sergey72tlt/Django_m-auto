735a9cc95f3611a83775d217cab39a7a41bbb49a
Sergey72tlt <94053785+Sergey72tlt@users.noreply.github.com>
2022-05-11 10:39:06 +0400

    Create new post

--- posts/views.py
+++ posts/views.py
@@ -5,12 +5,15 @@ from .models import Post
 
 
 def index(request):
-    post = Post.objects.annotate(favorite_nums=Count('favorite')).order_by('favorite_nums')[:10]
-    print(post.query)
+    posts = Post.objects.annotate(favorite_nums=Count('favorite')).order_by('favorite_nums')[:10]
+    responce = [f'id: {post.id}| author: {post.author}' for post in posts]
+    # print(post.query)
     return HttpResponse('Главная страница')
 
 
 def feed(request):
+    posts = Post.objects.filter(author__in=request.user.profile.friends.all())
+    responce = [f'id: {post.id}| author: {post.author}' for post in posts]
     return HttpResponse('Список объявлений')
 
 
