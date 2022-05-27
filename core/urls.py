0e3a8d9e8c0b036fbb78d9414245b443ab677efc
Sergey72tlt <94053785+Sergey72tlt@users.noreply.github.com>
2022-05-27 13:27:23 +0400

    Add CSS

--- core/urls.py
+++ core/urls.py
@@ -14,16 +14,19 @@ urlpatterns = [
     path('profile/<int:profile_id>/update/', ProfileUpdate.as_view(), name='profile-update'),
     path('subscribe/<int:user_id>', SubscribeView.as_view(), name='subscribe'),
 
-    path('password_reset/', PasswordResetView.as_view(template_name='core:password_reset.html',
-                                                      success_url=reverse_lazy('core:password-reset-done')),
-                                                      name='password-reset'),
+    path('password_reset/', PasswordResetView.as_view(template_name='core/password_reset.html',
+                                                      success_url=reverse_lazy('core:password-reset-done'),
+                                                      email_template_name='core/password_reset_email.html'
+                                                      ), name='password-reset'),
 
-    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='core:password_reset_done.html'),
-                                                                name='password-reset-done'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='core/password_reset_done.html'
+                                                               ), name='password-reset-done'),
     path('password_reset/<str:uidb64>/<slug:token>/',
          PasswordResetConfirmView.as_view(success_url=reverse_lazy('password-reset-complete'),
                                           template_name='core/password_reset_confirm.html'
                                           ), name='password_reset_confirm'),
 
-    path('password_reset/complete/', PasswordResetCompleteView.as_view(), name='password-reset-complete')
+    path('password_reset/complete/', PasswordResetCompleteView.as_view(
+                                                                       template_name='core/password_reset_complete.html'
+                                                                       ), name='password-reset-complete')
 ]
